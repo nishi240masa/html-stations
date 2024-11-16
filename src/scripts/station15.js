090c6adff86a5491de2061daaa4cf35af967e2cd
nishi240masa <masa2401106@gmail.com>
2024-11-16 18:46:58 +0900

    refactor: Convert returnThisData function to use arrow function syntax
    The returnThisData function in station13.js has been converted to use arrow function syntax for improved readability and consistency. This change ensures that the function returns the global data as intended. The code inside the function remains unchanged.

--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -3,14 +3,37 @@ async function getData() {
   // ３秒後にフルネームのリストを表示されるようにしましょう。
   // 最初から表示されていると、クリアになりません。
   const userList = [
-    { id: 1, first_name: '優', family_name: '大木', affiliation: 'TechTrain', is_student: false },
-    { id: 2, first_name: '太郎', family_name: '山田', affiliation: 'HogeHoge大学', is_student: true }
+    {
+      id: 1,
+      first_name: "優",
+      family_name: "大木",
+      affiliation: "TechTrain",
+      is_student: false,
+    },
+    {
+      id: 2,
+      first_name: "太郎",
+      family_name: "山田",
+      affiliation: "HogeHoge大学",
+      is_student: true,
+    },
   ];
-  const result = await test()
-  return await result
+  const result = new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(userList.map(buildFullName));
+    }, 3000);
+  });
+  return result;
 }
 
-function test() {
-  return
+function buildFullName(data) {
+  // Station14の問題はこちらです。想定する答えをここを書いてください。
+  // 氏名がわかるようにしつつ、半角スペースで繋いでください。
+  // またtest配列のそれぞれのオブジェクトに対して、full_nameのプロパティが追加されるように実装すること
+  const add = () => {
+    const full_name = data.family_name + " " + data.first_name;
+    data.full_name = full_name;
+  };
+  add();
+  return data;
 }
-
